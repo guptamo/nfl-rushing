02c3166a7838a338d7e4dacea30a04915d8be17b
mohit.gupta <mohit.gupta@openlane.com>
2020-02-26 07:38:56 -0500

    update tests

--- src/components/RushingTable/RushingTable.test.js
+++ src/components/RushingTable/RushingTable.test.js
@@ -20,6 +20,27 @@ describe('<RushingTable />', () => {
   })
 
   it('correctly calls setSortState when the Yrds header is clicked', () => {
+    let sortState = {
+      Yds: null,
+      Lng: null,
+      TD: null
+    }
+    let header
+
+    act(() => {
+      render(<RushingTable sortState={sortState} data={[]} setSortState={setSortStateSpy} />, container)
+      header = container.querySelectorAll("th.filterheader")[0]
+      header.dispatchEvent(new MouseEvent("click", { bubbles: true }))
+    })
+
+    expect(setSortStateSpy).toHaveBeenCalledWith({
+      Yds: 'asc',
+      TD: null,
+      Lng: null
+    })
+  })
+
+  it('correctly calls setSortState when the TD header is clicked', () => {
     const sortState = {
       Yds: null,
       Lng: null,
@@ -28,8 +49,146 @@ describe('<RushingTable />', () => {
     
     act(() => {
       render(<RushingTable sortState={sortState} data={[]} setSortState={setSortStateSpy} />, container)
-      const button = container.querySelector("th.filterheader:nth-child(0)")
-      button.dispatchEvent(new MouseEvent("click", { bubbles: true }))
+      const [_, header] = container.querySelectorAll("th.filterheader")
+      header.dispatchEvent(new MouseEvent("click", { bubbles: true }))
     })
+
+      expect(setSortStateSpy).toHaveBeenCalledWith({
+        Yds: null,
+        TD: 'asc',
+        Lng: null
+      })
+    })
+
+  it('correctly calls setSortState when the Lng header is clicked', () => {
+    const sortState = {
+      Yds: null,
+      Lng: null,
+      TD: null
+    }
+    
+    act(() => {
+      render(<RushingTable sortState={sortState} data={[]} setSortState={setSortStateSpy} />, container)
+      const header = container.querySelectorAll("th.filterheader")[2]
+      header.dispatchEvent(new MouseEvent("click", { bubbles: true }))
+    })
+
+    expect(setSortStateSpy).toHaveBeenCalledWith({
+      Yds: null,
+      TD: null,
+      Lng:'asc'
+    })
+  })
+
+  it('correctly transitions sort directions from null to "asc"', () => {
+    const sortState = {
+      Yds: null,
+      Lng: null,
+      TD: null
+    }
+    
+    act(() => {
+      render(<RushingTable sortState={sortState} data={[]} setSortState={setSortStateSpy} />, container)
+      const [Yds] = container.querySelectorAll("th.filterheader")
+      Yds.dispatchEvent(new MouseEvent("click", { bubbles: true }))
+    })
+
+    expect(setSortStateSpy).toHaveBeenNthCalledWith(1,{
+      Yds: 'asc',
+      TD: null,
+      Lng: null
+    })
+  })
+
+  it('correctly transitions sort directions from "asc" to "desc"', () => {
+    const sortState = {
+      Yds: 'asc',
+      Lng: null,
+      TD: null
+    }
+    
+    act(() => {
+      render(<RushingTable sortState={sortState} data={[]} setSortState={setSortStateSpy} />, container)
+      const [Yds] = container.querySelectorAll("th.filterheader")
+      Yds.dispatchEvent(new MouseEvent("click", { bubbles: true }))
+    })
+
+    expect(setSortStateSpy).toHaveBeenCalledWith({
+      Yds: 'desc',
+      TD: null,
+      Lng: null
+    })
+  })
+
+  it('correctly transitions sort directions from "desc" to null', () => {
+    const sortState = {
+      Yds: 'desc',
+      Lng: null,
+      TD: null
+    }
+    
+    act(() => {
+      render(<RushingTable sortState={sortState} data={[]} setSortState={setSortStateSpy} />, container)
+      const [Yds] = container.querySelectorAll("th.filterheader")
+      Yds.dispatchEvent(new MouseEvent("click", { bubbles: true }))
+    })
+
+    expect(setSortStateSpy).toHaveBeenCalledWith({
+      Yds: null,
+      TD: null,
+      Lng: null
+    })
+  })
+
+  it('renders all the players', () => {
+    const sortState = {
+      Yds: null,
+      Lng: null,
+      TD: null
+    }
+    const data = [
+      {
+        "Player":"Joe Banyard",
+        "Team":"JAX",
+        "Pos":"RB",
+        "Att":2,
+        "Att/G":2,
+        "Yds":7,
+        "Avg":3.5,
+        "Yds/G":7,
+        "TD":0,
+        "Lng":"7",
+        "1st":0,
+        "1st%":0,
+        "20+":0,
+        "40+":0,
+        "FUM":0
+      },
+      {
+        "Player":"Shaun Hill",
+        "Team":"MIN",
+        "Pos":"QB",
+        "Att":5,
+        "Att/G":1.7,
+        "Yds":5,
+        "Avg":1,
+        "Yds/G":1.7,
+        "TD":0,
+        "Lng":"9",
+        "1st":0,
+        "1st%":0,
+        "20+":0,
+        "40+":0,
+        "FUM":0
+      }
+    ]
+    
+    let rows
+    act(() => {
+      render(<RushingTable sortState={sortState} data={data} setSortState={setSortStateSpy} />, container)
+      rows = container.querySelectorAll("tbody tr")
+    })
+
+    expect(rows.length).toBe(2)
   })
 })
